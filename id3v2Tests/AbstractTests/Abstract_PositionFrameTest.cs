0d1747e674591dff7c62b0ae76af40c4a41e071d
elp87 <elp1987@gmail.com>
2013-09-25 13:41:21 +0400

    Testing "PositionFrame"

--- id3v2Tests/AbstractTests/Abstract_PositionFrameTest.cs
+++ id3v2Tests/AbstractTests/Abstract_PositionFrameTest.cs
@@ -11,12 +11,16 @@ namespace id3v2Tests.AbstractTests
         FrameFlagSet ffs;
         PositionFrame test;
         byte[] byte01_15_slash;
+        byte[] byte1_07_dot;
+        byte[] byte01;
 
         public Abstract_PositionFrameTest()
         {
             ffs = new FrameFlagSet(new byte[] { 0x00, 0x00 });
             byte01_15_slash = new byte[] { 0x00, 0x30, 0x31, 0x2F, 0x31, 0x35 };
-            test = new PositionFrame(ffs, byte01_15_slash);
+            byte1_07_dot = new byte[] { 0x00, 0x31, 0x2E, 0x30, 0x37 };
+            byte01 = new byte[] { 0x00, 0x30, 0x31 };
+
         }
 
         [TestMethod]
@@ -26,9 +30,51 @@ namespace id3v2Tests.AbstractTests
             int expNumber = 1;
             int expTotalNumber = 15;
 
+            test = new PositionFrame(ffs, byte01_15_slash);
+
             Assert.AreEqual(expNumericString, test.numericString);
             Assert.AreEqual(expNumber, test.number);
             Assert.AreEqual(expTotalNumber, test.totalNumber);
+        }
+
+        [TestMethod]
+        public void TestDot()
+        {
+            string expNumericString = "1.07";
+            int expNumber = 7;
+            int expTotalNumber = -1;
+
+            test = new PositionFrame(ffs, byte1_07_dot);
+
+            Assert.AreEqual(expNumericString, test.numericString);
+            Assert.AreEqual(expNumber, test.number);
+            Assert.AreEqual(expTotalNumber, test.totalNumber);
+        }
+
+        [TestMethod]
+        public void TestUsual()
+        {
+            string expNumericString = "01";
+            int expNumber = 1;
+            int expTotalNumber = -1;
+
+            test = new PositionFrame(ffs, byte01);
+
+            Assert.AreEqual(expNumericString, test.numericString);
+            Assert.AreEqual(expNumber, test.number);
+            Assert.AreEqual(expTotalNumber, test.totalNumber);
+        }
+
+        [TestMethod]
+        public void TestParseNumericString()
+        {
+            int expNumber = 1;
+
+            PrivateObject target = new PrivateObject(typeof(PositionFrame));
+            target.Invoke("ParseNumericString", new object[] { "01" });
+            int number = (int)target.GetProperty("number");
+
+            Assert.AreEqual(expNumber, number);
         }
     }
 }
