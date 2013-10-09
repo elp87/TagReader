c23f24022a6f8796fc36acb91e58a2d5420ce314
elp87 <elp1987@gmail.com>
2013-10-09 18:28:40 +0400

    Add class "DateInfoFrame"

---
+++ elp87.TagReader/id3v2/Frames/DateInfoFrame.cs
@@ -0,0 +1,60 @@
+﻿using elp87.TagReader.id3v2.Frames;
+using System;
+using System.Globalization;
+
+namespace elp87.TagReader.id3v2.Frames
+{
+    public class DateInfoFrame
+        : TextInfoFrame        
+    {
+        #region Fields
+        private DateTime _date; 
+        #endregion
+
+        #region Constructors
+        protected DateInfoFrame()
+        { }
+
+        public DateInfoFrame(FrameFlagSet flags, byte[] frameData)
+            : base(flags, frameData)
+        {
+            _date = this.ParseDate();
+        }        
+        #endregion
+
+        #region Properties
+        public DateTime Date { get { return this._date; } }
+        #endregion
+
+        #region Methods
+        private DateTime ParseDate()
+        {
+            return this.ParseDate(this.ToString());
+        }
+
+        private DateTime ParseDate(string dateString)
+        {
+            string dateFormat;
+            switch (dateString.Length)
+            {
+                case 4:
+                    dateFormat = "yyyy"; break;
+                case 7:
+                    dateFormat = @"yyyy-MM"; break;
+                case 10:
+                    dateFormat = @"yyyy-MM-dd"; break;
+                case 13:
+                    dateFormat = @"yyyy-MM-ddTHH"; break;
+                case 16:
+                    dateFormat = @"yyyy-MM-ddTHH:mm"; break;
+                case 19:
+                    dateFormat = @"yyyy-MM-ddTHH:mm:ss"; break;
+                default:
+                    throw new FormatException();
+            }
+            return DateTime.ParseExact(dateString, dateFormat, CultureInfo.InvariantCulture); 
+        }
+        
+        #endregion
+    }
+}
---
+++ id3v2Tests/Frames/Frames_DateInfoFrameTest.cs
@@ -0,0 +1,53 @@
+﻿using elp87.TagReader.id3v2;
+using elp87.TagReader.id3v2.Frames;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace id3v2Tests.Frames
+{
+    [TestClass]
+    public class Frames_DateInfoFrameTest
+    {
+        [TestMethod]
+        public void TestYYYYMMDD()
+        {
+            FrameFlagSet ffs = new FrameFlagSet(new byte[] { 0x00, 0x00 });
+            byte[] data = new byte[]
+            {
+                0x00, 0x32, 0x30, 0x31, 0x30, 0x2D, 0x30, 0x31, 0x2D, 0x30, 0x31
+            };
+
+            DateInfoFrame test = new DateInfoFrame(ffs, data);
+        }
+
+        [TestMethod]
+        public void TestParseDate()
+        {
+            DateTime[] expValue = new DateTime[]
+            {
+                new DateTime(2013, 1, 1),
+                new DateTime(2013, 10, 9),
+                new DateTime(2013, 10, 9, 18, 26, 00)
+            };
+            string[] inValues = new string[]
+            {
+                "2013", "2013-10-09", "2013-10-09T18:26"
+            };
+            DateTime[] values = new DateTime[inValues.Length];
+
+            PrivateObject target = new PrivateObject(typeof(DateInfoFrame));
+
+            for (int i = 0; i < inValues.Length; i++)
+            {
+                values[i] = (DateTime)target.Invoke("ParseDate", new object[] { inValues[i] });
+
+            }
+
+            Assert.AreEqual(expValue.Length, values.Length);
+            for (int i = 0; i < expValue.Length; i++)
+            {
+                Assert.AreEqual(expValue[i], values[i]);
+            }
+        }
+    }
+}
