af6de6930737d6f2c45dfca990debbbef8ec6eda
elp87 <elp1987@gmail.com>
2013-10-09 13:41:09 +0400

    Add class "UserDefinedTextFrame"

---
+++ elp87.TagReader/id3v2/Frames/UserDefinedTextFrame.cs
@@ -0,0 +1,29 @@
+﻿using elp87.TagReader.id3v2.Abstract;
+using System;
+
+namespace elp87.TagReader.id3v2.Frames
+{
+    public class UserDefinedTextFrame
+        : UserDefinedFrame
+    {
+        #region Constructors
+        protected UserDefinedTextFrame()
+            : base()
+        { }
+
+        public UserDefinedTextFrame(FrameFlagSet flags, byte[] frameData)
+            : base(flags, frameData)
+        { }
+        #endregion
+
+        #region Methods
+        protected override string ParseValue(byte[] frameData)
+        {
+            byte[] valueData = new byte[this._frameData.Length - this._terminatorPos - 1];
+            Array.Copy(this._frameData, _terminatorPos + 1, valueData, 0, valueData.Length);
+            string value = this.GetString(valueData);
+            return value;
+        } 
+        #endregion
+    }
+}
---
+++ id3v2Tests/Frames/Frames_UserDefinedTextFrameTest.cs
@@ -0,0 +1,24 @@
+﻿using elp87.TagReader.id3v2;
+using elp87.TagReader.id3v2.Frames;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace id3v2Tests.Frames
+{
+    [TestClass]
+    public class Frames_UserDefinedTextFrameTest
+    {
+        [TestMethod]
+        public void TestFrame()
+        {
+            string expDescription = "major_brand";
+            string expValue = "M4A ";
+
+            FrameFlagSet ffs = new FrameFlagSet(new byte[] { 0x00, 0x00 });
+            byte[] testArray = new byte[] { 0x00, 0x6D, 0x61, 0x6A, 0x6F, 0x72, 0x5F, 0x62, 0x72, 0x61, 0x6E, 0x64, 0x00, 0x4D, 0x34, 0x41, 0x20 };
+            UserDefinedTextFrame test = new UserDefinedTextFrame(ffs, testArray);
+
+            Assert.AreEqual(expDescription, test.description);
+            Assert.AreEqual(expValue, test.value);
+        }
+    }
+}
