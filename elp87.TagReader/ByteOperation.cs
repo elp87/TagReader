05ea055cfbb7b8cf0fcc0aac7f04a69ed4955323
elp87 <elp1987@gmail.com>
2013-10-23 15:14:46 +0400

    Document "ByteOperation" and "Generic" classes

--- elp87.TagReader/ByteOperation.cs
+++ elp87.TagReader/ByteOperation.cs
@@ -2,13 +2,58 @@
 
 namespace elp87.TagReader
 {
+    /// <summary>
+    /// Provides static methods for operations with bytes.
+    /// </summary>
     public static class ByteOperation
     {
+        /// <summary>
+        /// Returns value of bit within byte.
+        /// </summary>
+        /// <param name="initByte">Byte</param>
+        /// <param name="bitPosition">The zero-based index position of bit within byte. Position 0 is the least significant bit, position 7 is the most significant bit</param>
+        /// <returns>Boolean value of bit</returns>
+        /// <example>
+        /// <code lang="C#">
+        /// using elp87.TagReader;
+        /// using System;
+        /// namespace Example
+        /// {
+        ///     class Program
+        ///     {
+        ///         static void Main(string[] args)
+        ///         {
+        ///             byte init = 35; // 0010 0011
+        ///             for (int i = 0; i &lt;= 8; i++)
+        ///             {
+        ///                 bool result = ByteOperation.GetBit(init, i);
+        ///                 Console.WriteLine(result);
+        ///             }
+        ///         }
+        ///     }
+        /// }
+        /// // The example displays the following output:
+        /// // True
+        /// // True
+        /// // False
+        /// // False
+        /// // False
+        /// // True
+        /// // False
+        /// // False
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// If bit position is out of byte, method returns "false".
+        /// <code lang="C#">
+        /// bool result = ByteOperation.GetBit(init, 9);
+        /// </code>
+        /// </remarks>
         public static bool GetBit(byte initByte, int bitPosition)
         {
             double order = Convert.ToDouble(bitPosition);
             int mask = (int)Math.Pow(2, order);
             return Convert.ToBoolean((initByte & mask) >> bitPosition);
-        }        
+        }
     }
 }
--- elp87.TagReader/Generic.cs
+++ elp87.TagReader/Generic.cs
@@ -4,8 +4,52 @@ using System.Text;
 
 namespace elp87.TagReader
 {
+    /// <summary>
+    /// Provides static generic methods
+    /// </summary>
     public static class Generic
     {
+        /// <summary>
+        /// Adds element in the end of array and returns new instance of array.
+        /// </summary>
+        /// <typeparam name="T">The type of array elements.</typeparam>
+        /// <param name="array">Initial array</param>
+        /// <param name="element">Element that should be added to array</param>
+        /// <returns>New instance of array with added element in the end</returns>
+        /// <example>
+        /// <code lang="C#">
+        /// using elp87.TagReader;
+        /// using System;
+        /// 
+        /// namespace Example
+        /// {
+        ///     class Program
+        ///     {
+        ///         static void Main(string[] args)
+        ///         {
+        ///             int[] array = new int[] { 0, 1, 2, 3, 4 };
+        ///             int[] newArray = Generic.Add&lt;int&gt;(array, 5);
+        /// 
+        ///             foreach (int num in newArray)
+        ///             {
+        ///                 Console.Write("{0}, ", num);
+        ///             }
+        ///         }
+        ///     }
+        /// }
+        /// // The example displays the following output:
+        /// // 0, 1, 2, 3, 4, 5,
+        /// </code>
+        /// It is not necessarily to indicate type of elements, but <c>array</c> and <c>element</c> should be the same type
+        /// <code lang="C#">
+        /// int[] newArray = Generic.Add(array, 5);
+        /// </code>
+        /// You can get new array as initial one.
+        /// <code lang="C#">
+        /// int[] array = new int[] { 0, 1, 2, 3, 4 };
+        /// array = Generic.Add(array, 5);
+        /// </code>
+        /// </example>
         public static T[] Add<T>(T[] array, T element)
         {
             T[] temp = new T[array.Length];
@@ -13,7 +57,7 @@ namespace elp87.TagReader
             T[] newArray = new T[temp.Length + 1];
             for (int i = 0; i < temp.Length; i++) { newArray[i] = temp[i]; }
             newArray[newArray.Length - 1] = element;
-            return newArray;            
+            return newArray;
         }
     }
 }
